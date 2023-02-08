a86d9bd99c550ebe53cd4c95964c4b3a14a1f8e8
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-02-08 20:48:37 +0100

    Ejercicio 20.3 resuelto

--- src/20_DOM/3_readHereWriteThere/resposta.js
+++ src/20_DOM/3_readHereWriteThere/resposta.js
@@ -4,6 +4,19 @@
  */
 
 // Escribe aquí o teu código.
+const entradaDoTexto=document.querySelector("textarea")
+const saidaDoResultado=document.querySelector("p#enMaiusculas")
+
+function actualizaSaidaEnMaiusculas(){
+    const textoIntroducido = entradaDoTexto.value
+    const textoEnMaiusculas =  textoIntroducido.toUpperCase()
+    saidaDoResultado.innerText = textoEnMaiusculas
+    /* Las lineas 11 a 13 se podrían escribir en una sola linea de la siguiente forma: 
+    saidaDoResultado.innerText = entradaDoTexto.value.toUpperCase()
+    */
+}
+
+entradaDoTexto.addEventListener("input", actualizaSaidaEnMaiusculas)
 
 /**
  * Non cambies nada despois destas liñas
