1f32966dbb981661fb98d06aec9202431788a4f0
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-02-09 17:53:10 +0100

    Exercise 20.4 done

--- src/20_DOM/4_multipleTasks/resposta.js
+++ src/20_DOM/4_multipleTasks/resposta.js
@@ -4,6 +4,26 @@
  */
 
 // Escribe aquí o teu código.
+const entradaDoTexto = document.querySelector("textarea")
+const saidaNumeroCaracteres = document.querySelector("#numeroCaracteres")
+const saidaNumeroPalabras = document.querySelector("#numeroPalabras")
+
+function actualizaNumeroCaracteres(){
+    const obterString = entradaDoTexto.value
+    const lonxitudeString = obterString.length
+    saidaNumeroCaracteres.innerText =  lonxitudeString
+}
+
+entradaDoTexto.addEventListener("input", actualizaNumeroCaracteres)
+
+function actualizaNumeroPalabras(){
+    const obterString = entradaDoTexto.value
+    const stringFragmentado = obterString.split(" ")
+    const numeroPalabras = stringFragmentado.length
+    saidaNumeroPalabras.innerText = numeroPalabras
+}
+
+entradaDoTexto.addEventListener("input", actualizaNumeroPalabras)
 
 /**
  * Non cambies nada despois destas liñas
