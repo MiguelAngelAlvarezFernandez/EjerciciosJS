40e0bbbd995ae8a8e7bfd6fa40a3cb75012e72e1
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-02-09 20:49:17 +0100

    Excercise 6.1 done

--- src/6_flow_control/1_if/1_toDoOrNotToDo/index.js
+++ src/6_flow_control/1_if/1_toDoOrNotToDo/index.js
@@ -20,6 +20,15 @@
  */
 
 // Your code here
+function divisionDe(n1, n2) {
+    if(
+        typeof(n1) === "number"
+        && typeof(n2) === "number"
+        && n2 !== 0
+    ) {
+        return n1/n2
+    }
+}
 
 /**
  * Don't to change next lines.
